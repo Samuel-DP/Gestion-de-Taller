fee6e58f9076f64ac9ddb4664fcc95988314cd49
samuel <samuel1122000@gmail.com>
2025-03-26 11:48:31 +0100

    Empezar

---
+++ Gestion-de-taller/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gestion de taller");
+        System.out.println("1. Registrar cliente");
+        System.out.println("2. Registrar vehiculo");
+        System.out.println("3. Crear servicio");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+}
---
+++ Gestion-de-taller/src/Vehiculos.java
@@ -0,0 +1,19 @@
+public class Vehiculos{
+
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private int año;
+    private int km;
+    private String nºBastidor;
+
+    public Vehiculos(String matricula,String marca, String modelo, int año, int km, String nºBastidor  ){
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.km = km;
+        this.nºBastidor = nºBastidor;
+    }
+    
+}
