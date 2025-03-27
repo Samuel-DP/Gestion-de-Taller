51f5ed28187d28a52c751763fe5ea2b7c05921ab
andrestoma <qndrestoma@gmail.com>
2025-03-27 10:56:42 +0100

    Crear metodos basicos

--- Gestion-de-taller/src/App.java
+++ Gestion-de-taller/src/App.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        
+        int opcion;
+        do{ 
+        Taller taller = new Taller();
         System.out.println("Gestion de taller");
         System.out.println("1. Registrar cliente");
         System.out.println("2. Registrar vehiculo");
@@ -12,7 +16,22 @@ public class App {
         System.out.println("");
         System.out.println("");
         System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("8. Salir");
+        System.out.println("Selecciona una opcion: ");
+        
+        opcion = scanner.nextInt();
+
+        
+        switch (opcion) {
+            case 1 -> taller.crearCliente();
+            case 2 -> taller.crearVehiculo();
+            case 3 -> taller.crearServicio();
+            case 8 -> System.out.println("Saliendo del programa");   
+            default -> System.out.println("Opción no válida, intente nuevamente.");        
+        }   
+            
+        }while(opcion != 8);
+        
+        
     }
 }
--- Gestion-de-taller/src/Taller.java
+++ Gestion-de-taller/src/Taller.java
@@ -1,3 +1,68 @@
+
+import java.util.Scanner;
+
+
 public class Taller {
+    Scanner scanner = new Scanner(System.in);
     
+    public Cliente crearCliente(){
+        String nombreCliente;
+        String apellidoCliente;
+        String dniCliente;
+        int telefonoCliente;
+
+        System.out.println("Ingresa un nombre: ");
+        nombreCliente = scanner.nextLine();
+        System.out.println("Ingresa un apellido: ");
+        apellidoCliente = scanner.nextLine();
+        System.out.println("Ingrese el DNI: ");
+        dniCliente = scanner.nextLine();
+        System.out.println("Ingrese el teléfono: ");
+        telefonoCliente = scanner.nextInt();
+
+
+        Cliente cliente = new Cliente(nombreCliente, apellidoCliente, dniCliente, telefonoCliente);
+        return cliente;
+    }
+
+    public Vehiculo crearVehiculo(){
+        String matriculaVehiculo;
+        String marcaVehiculo;
+        String modeloVehiculo;
+        int añoVehiculo;
+        int kmVehiculo;
+        String nBastidorVehiculo;
+
+        System.out.println("Ingrese una matricula: ");
+        matriculaVehiculo = scanner.nextLine();
+        System.out.println("Ingrese una marca: ");
+        marcaVehiculo = scanner.nextLine();
+        System.out.println("Ingrese un modelo: ");
+        modeloVehiculo = scanner.nextLine();
+        System.out.println("Ingrese un año: ");
+        añoVehiculo = scanner.nextInt();        
+        System.out.println("Ingrese kms del vehiculo: ");
+        kmVehiculo = scanner.nextInt();
+        System.out.println("Ingrese el numero de bastidor: ");
+        nBastidorVehiculo = scanner.nextLine();
+
+        Vehiculo vehiculo = new Vehiculo(matriculaVehiculo, marcaVehiculo, modeloVehiculo, añoVehiculo, kmVehiculo, nBastidorVehiculo);
+        return vehiculo;
+    }
+
+    public Servicio crearServicio(){
+        String descripcionServicio;
+        Double precioServicio;
+        Double horasServicio;
+       
+        System.out.println("Ingrese una descripcion del servicio: ");
+        descripcionServicio = scanner.nextLine();
+        System.out.println("Ingrese un precio por h del servicio: ");
+        precioServicio = scanner.nextDouble();
+        System.out.println("Ingrese las horas estimadas del servicio: ");
+        horasServicio = scanner.nextDouble();
+
+        Servicio servicio = new Servicio(descripcionServicio, precioServicio, horasServicio);
+        return servicio;
+    }
 }
