70c16e669fc432b1d1a64f8583336bd778f61156
samuel <samuel1122000@gmail.com>
2025-03-27 12:02:43 +0100

    Conexion a base de datos establecida

--- Gestion-de-taller/src/App.java
+++ Gestion-de-taller/src/App.java
@@ -1,18 +1,25 @@
+import java.sql.Connection;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Gestion de taller");
         System.out.println("1. Registrar cliente");
         System.out.println("2. Registrar vehiculo");
         System.out.println("3. Crear servicio");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+
+        System.out.println("Hola");
+
+        Connection conexion = ConexionBD.conectar();
+        if (conexion != null) {
+        System.out.println("Conexión establecida correctamente.");
+        } else {
+        System.out.println("No se pudo establecer la conexión.");
+        }
+    
     }
 }
+
+
